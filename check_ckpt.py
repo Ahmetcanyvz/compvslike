71b070ef3a4114d63c0d66d1e124cd2d1df7384b
Ahmetcanyvz <yavuzahmetcan1@gmail.com>
2026-03-24 14:16:47 +0100

    Show full fit_loop state in checkpoint inspector

--- check_ckpt.py
+++ check_ckpt.py
@@ -1,7 +1,11 @@
-import sys, torch
+import sys, json, torch
 ckpt = torch.load(sys.argv[1], map_location='cpu', weights_only=False)
 print('Keys:', sorted(ckpt.keys()))
-if 'loops' in ckpt: print('Loops:', list(ckpt['loops'].keys()))
-if 'global_step' in ckpt: print('Global step:', ckpt['global_step'])
-if 'optimizer_states' in ckpt: print('Optimizer saved: yes')
-if 'lr_schedulers' in ckpt: print('LR scheduler saved: yes')
+print('Global step:', ckpt.get('global_step'))
+print('Epoch:', ckpt.get('epoch'))
+print('Optimizer saved:', 'optimizer_states' in ckpt)
+print('LR scheduler saved:', 'lr_schedulers' in ckpt)
+print()
+print('=== fit_loop ===')
+fit = ckpt.get('loops', {}).get('fit_loop', {})
+print(json.dumps(fit, indent=2, default=str))

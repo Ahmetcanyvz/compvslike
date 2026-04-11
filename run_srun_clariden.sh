e518368a92f56b9043b5194681e6bd0b82337ca5
Ahmetcanyvz <yavuzahmetcan1@gmail.com>
2026-04-11 15:48:20 +0200

    Reduce batch_size to 16 for GH200 95GB

--- run_srun_clariden.sh
+++ run_srun_clariden.sh
@@ -32,9 +32,9 @@ model:
 training:
   seed: ${SEED}
   max_tokens: 20000000000
-  batch_size: 32
+  batch_size: 16
   eval_batch_size: 4
-  gradient_accumulation: 2
+  gradient_accumulation: 4
   sequence_length: 2048
   learning_rate: 6.0e-4
   weight_decay: 0.1

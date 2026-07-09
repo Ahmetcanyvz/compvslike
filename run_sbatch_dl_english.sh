96af10c5bc9ca335389f565aef38ca9809f4184e
Ahmetcanyvz <yavuzahmetcan1@gmail.com>
2026-07-09 23:30:03 +0200

    Cap download job time limits at 12h partition max

--- run_sbatch_dl_english.sh
+++ run_sbatch_dl_english.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=dl-eng
 #SBATCH --partition=normal
 #SBATCH --account=a0229
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=288
